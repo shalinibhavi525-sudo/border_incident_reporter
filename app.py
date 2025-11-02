3fb5739517da2b067b7895a941754cb8e0f57f72
shalinibhavi525-sudo <shalinibhavi525@gmail.com>
2025-11-02 12:45:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -193,4 +193,5 @@ def get_stats():
 # ==================== RUN APP ====================
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(debug=False, host='0.0.0.0', port=port)
